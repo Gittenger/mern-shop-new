fdce646dfa1293b96253286f3d942e7baaf5b047
Gittenger <36045366+Gittenger@users.noreply.github.com>
2021-06-01 21:06:33 -0400

    jwt to package.json, userRoutes in app

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
+const userRoutes = require('./routes/userRoutes')
 
 const app = express()
 
@@ -11,5 +12,7 @@ app.use((req, res, next) => {
 	next()
 })
 app.use(cors())
+
+app.use('/', userRoutes)
 
 module.exports = app
