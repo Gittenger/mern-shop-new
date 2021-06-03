6260965fd0d7b42c0f4ce2c0ff9eb160f501dba0
Gittenger <36045366+Gittenger@users.noreply.github.com>
2021-06-02 21:36:22 -0400

    get me function

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,15 +2,18 @@ const express = require('express')
 const authController = require('../controllers/authController')
 const userController = require('../controllers/userController')
 
-const { signup, login, protect, restrictTo } = authController
-const { getOne } = userController
+const { signup, login, protect, restrictTo, logout } = authController
+const { getOne, getMe } = userController
 
 const router = express.Router()
 
 router.post('/signup', signup)
 router.post('/login', login)
+router.post('/logout', logout)
 
 router.use(protect)
+
+router.get('/getMe', getMe, getOne)
 
 router.get('/getUser/:id', restrictTo('admin'), getOne)
 
