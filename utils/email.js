2a47b0716d196dc1ef70da0e2f18c3e896286cec
Gittenger <36045366+Gittenger@users.noreply.github.com>
2021-06-08 00:05:22 -0400

    email util added

---
+++ utils/email.js
@@ -0,0 +1,59 @@
+const nodemailer = require('nodemailer')
+const nodemailerSendgrid = require('nodemailer-sendgrid')
+const htmlToText = require('html-to-text')
+const pug = require('pug')
+
+module.exports = class Email {
+	constructor(user, url) {
+		this.to = user.email
+		this.url = url
+		this.from = process.env.EMAIL_FROM || 'defaultAdmin@test.com'
+	}
+
+	newTransport() {
+		if (process.env.NODE_ENV == 'production') {
+			// use sendgrid
+			return nodemailer.createTransport(
+				nodemailerSendgrid({ apiKey: process.env.SENDGRID_API_KEY })
+			)
+		}
+
+		return nodemailer.createTransport({
+			service: process.env.EMAIL_HOST,
+			port: process.env.EMAIL_PORT,
+			auth: {
+				user: process.env.EMAIL_USERNAME,
+				pass: process.env.EMAIL_PASSWORD,
+			},
+		})
+	}
+
+	async send(template, subject) {
+		const html = pug.renderFile(`${__dirname}/mailTemplates/${template}.pug`, {
+			url: this.url,
+			subject,
+		})
+
+		const mailOptions = {
+			from: this.from,
+			to: this.to,
+			// other options and local vars here
+			subject,
+			html,
+			text: htmlToText.htmlToText(html),
+		}
+
+		await this.newTransport().sendMail(mailOptions)
+	}
+
+	async sendWelcome() {
+		await this.send('welcome', 'Welcome to our site!')
+	}
+
+	async sendPasswordReset() {
+		await this.send(
+			'resetPassword',
+			'Your password reset token (expires in 10 minutes)'
+		)
+	}
+}
